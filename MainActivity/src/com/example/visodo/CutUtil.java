e8241d0da9e23a389236879ef181e57b21ef7078
oo1993448102 <19931123xue@163.com>
2016-05-18 10:35:52 +0800

    CutUtil

---
+++ MainActivity/src/com/example/visodo/CutUtil.java
@@ -0,0 +1,12 @@
+package com.example.visodo;
+
+public class CutUtil {
+	
+	public CutUtil(){}
+	
+	public static String Cut(String num){
+		String cuttedString = num.substring(0, 5);
+		return cuttedString;
+	}
+
+}
--- sdk/java/gen/org/opencv/engine/OpenCVEngineInterface.java
+++ sdk/java/gen/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/EchoZhou/develop/opencv/sdk/java/src/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\EchoZhou\\git\\mvo_android\\sdk\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
