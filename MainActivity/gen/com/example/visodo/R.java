e316b9ad18fc2a7e404c12bf6fae04cf69188004
oo1993448102 <19931123xue@163.com>
2016-05-18 10:57:10 +0800

    read local path

--- MainActivity/gen/com/example/visodo/R.java
+++ MainActivity/gen/com/example/visodo/R.java
@@ -71,6 +71,7 @@ containing a value of this type.
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int path=0x7f050003;
     }
     public static final class style {
         /** 
